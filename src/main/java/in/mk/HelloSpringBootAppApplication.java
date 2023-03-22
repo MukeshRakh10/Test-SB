13d7e8df468b86caab19f7e41f6cc9b64e314a01
MukeshRakh <mukesh10.rakh@gmail.com>
2023-03-23 01:51:57 +0530

    test

---
+++ src/main/java/in/mk/HelloSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package in.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringBootAppApplication.class, args);
+	}
+
+}
